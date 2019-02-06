9f4306e3f1cfd1405b70d778f25e4253a68eeab7
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-06 20:42:59 +0300

    update MainActivity.java - add Toast and commits
    update strings.xml
    - add Toast warning create order

--- app/src/main/java/com/number7/udemycafeorder/MainActivity.java
+++ app/src/main/java/com/number7/udemycafeorder/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,18 +21,27 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.editTextPassword);
     }
 
+    /*
+    * Обработка кнопки "Отправить заказ"
+    * Передает имя и пароль пользователя
+    *
+    * Проверка: если имя и пароль не пустые, передать данные в активность CreateOrderActivity
+    * иначе: вывести короткое всплывающее уведомление
+    * */
     public void onClickCreateOrderMain(View view) {
 
         String name = editTextName.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
+        // проверка
         if(!name.isEmpty() && !password.isEmpty()){
             Intent intent = new Intent(this,CreateOrderActivity.class);
             intent.putExtra("name", name);
             intent.putExtra("password",password);
             startActivity(intent);
         }else{
-
+            // уведомление
+            Toast.makeText(this,R.string.Main_toast_create_order, Toast.LENGTH_SHORT).show();
         }
 
     }
