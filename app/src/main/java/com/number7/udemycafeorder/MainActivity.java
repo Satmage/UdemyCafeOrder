a6ddd12bd7ef62bcd86baedee152c1e3868f7998
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-06 20:27:43 +0300

    add CreateOrderActivity
    edit MainActivity.java
    - add button handler: sending name and password

--- app/src/main/java/com/number7/udemycafeorder/MainActivity.java
+++ app/src/main/java/com/number7/udemycafeorder/MainActivity.java
@@ -1,13 +1,38 @@
 package com.number7.udemycafeorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+    }
+
+    public void onClickCreateOrderMain(View view) {
+
+        String name = editTextName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(!name.isEmpty() && !password.isEmpty()){
+            Intent intent = new Intent(this,CreateOrderActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("password",password);
+            startActivity(intent);
+        }else{
+
+        }
+
     }
 }
