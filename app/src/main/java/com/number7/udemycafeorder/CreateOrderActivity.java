9cd7d7a47ae75d0ea56e1a9e7ff00d5c2c4d4338
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-07 19:42:23 +0300

    update CreateOrderActivity.java
    update OrderDetailActivity.java
    - add handler content
    
    update activity_order_detail.xml
    - add TextViewOrder
    - add ScrollView
    
    update string.xml

--- app/src/main/java/com/number7/udemycafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/number7/udemycafeorder/CreateOrderActivity.java
@@ -28,10 +28,18 @@ public class CreateOrderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_create_order);
+        // присвоить переменой drink значение сроковой переменной
         drink = getString(R.string.createOrder_radioButton_tea);
-
+        // Получить данные с интента (предыдущей активности)
         Intent intent = getIntent();
+
+        /*
+         * Если содержится в интенте "name" и "password"
+         * присвоить переменным значения интента
+         * иначе
+         * присвоить переменным name и password значения по умолчанию
+         * */
         if (intent.hasExtra("name") && intent.hasExtra("password")) {
             name = intent.getStringExtra("name");
             password = intent.getStringExtra("password");
@@ -39,9 +47,6 @@ public class CreateOrderActivity extends AppCompatActivity {
             name = getString(R.string.default_name);
             password = getString(R.string.default_password);
         }
-
-
-        setContentView(R.layout.activity_create_order);
 
         textViewHello = findViewById(R.id.textViewHello);
         textViewAddition = findViewById(R.id.textViewAddition);
@@ -59,6 +64,9 @@ public class CreateOrderActivity extends AppCompatActivity {
         spinnerTea = findViewById(R.id.spinnerTea);
         spinnerCoffee = findViewById(R.id.spinnerCoffee);
 
+        /*
+         * Применяется при необходимости множества изменений в строке символов
+         * */
         builderAddition = new StringBuilder();
 
     }
@@ -66,12 +74,20 @@ public class CreateOrderActivity extends AppCompatActivity {
     public void onClickChangeDrinkRadioButton(View view) {
         RadioButton button = (RadioButton) view;
         int id = button.getId();
-        if(id == R.id.radioButtonTea){
+        /*
+         * Если id равен id кнопки radioButtonTea то
+         * drink - принимает значение строковой переменной tea
+         * spinnerTea - становится видимым
+         * spinnerCoffee - становится невидимым
+         * checkBoxLemon - становится видимым
+         * иначе...
+         * */
+        if (id == R.id.radioButtonTea) {
             drink = getString(R.string.createOrder_radioButton_tea);
             spinnerTea.setVisibility(View.VISIBLE);
             spinnerCoffee.setVisibility(View.INVISIBLE);
             checkBoxLemon.setVisibility(View.VISIBLE);
-        } else if (id == R.id.radioButtonCoffee){
+        } else if (id == R.id.radioButtonCoffee) {
             drink = getString(R.string.createOrder_radioButton_coffee);
             spinnerCoffee.setVisibility(View.VISIBLE);
             spinnerTea.setVisibility(View.INVISIBLE);
@@ -84,29 +100,57 @@ public class CreateOrderActivity extends AppCompatActivity {
     }
 
     public void onClickSendOrderImage(View view) {
+        // обнуляем переменную buildAddition
         builderAddition.setLength(0);
-        if(checkBoxMilk.isChecked()){
+        // если checkbox нажат то имя добавляется в переменную buildAddition
+        if (checkBoxMilk.isChecked()) {
             builderAddition.append(getString(R.string.createOrder_checkBox_milk)).append(" ");
         }
-        if(checkBoxLemon.isChecked()){
-            builderAddition.append(getString(R.string.createOrder_checkBox_lemon)).append(" ");
-        }
-        if(checkBoxSugar.isChecked() && drink.equals(getString(R.string.createOrder_radioButton_tea))){
+        if (checkBoxSugar.isChecked()) {
             builderAddition.append(getString(R.string.createOrder_checkBox_sugar)).append(" ");
         }
-        String optionOfDrink = " ";
-        if(drink.equals(getString(R.string.createOrder_radioButton_tea))){
+        if (checkBoxLemon.isChecked() && drink.equals(getString(R.string.createOrder_radioButton_tea))) {
+            builderAddition.append(getString(R.string.createOrder_checkBox_lemon)).append(" ");
+        }
+        // optionOfDrink - содержит опции напитка
+        String optionOfDrink;
+        /*
+         * Если содержимое dink эквивалентно строковой переменной createOrder_radioButton_tea
+         * то - optionOfDrink присваивается значение всплывающего списка SpinnerTea
+         * иначе - всплывающего списка coffee
+         * */
+        if (drink.equals(getString(R.string.createOrder_radioButton_tea))) {
+            //getSelectedItem() - выбаранный элемент всплывающего списка
+            // toString() - преобразовать в строку
             optionOfDrink = spinnerTea.getSelectedItem().toString();
-        }else{
+        } else {
             optionOfDrink = spinnerCoffee.getSelectedItem().toString();
         }
-        String order = String.format(getString(R.string.order),name,password,drink, optionOfDrink);
+        // order - переменная со значением: Имя/пароль/напиток/категория напитка
+        String order = String.format(getString(R.string.order), name, password, drink, optionOfDrink);
+        // addition - переменная со значением добавок
         String additions;
-        if(builderAddition.length() > 0){
+        /*
+         * Если длина строки в builderAddition > 0 то
+         * в переменную additions заносятся добавки
+         * иначе additions остается пустым
+         * */
+        if (builderAddition.length() > 0) {
             additions = getString(R.string.need_addition) + builderAddition.toString();
-        }else{
+        } else {
             additions = " ";
         }
+
+        /*
+         * fullOrder - содержит полный заказ:
+         * Имя/Пароль/Напиток/Категория напитка/Добавки
+         */
+
         String fullOrder = order + additions;
+
+        // при нажатии на картинку переход к OrderDetailActivity активности
+        Intent intent = new Intent(this, OrderDetailActivity.class);
+        intent.putExtra("order", fullOrder);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/number7/udemycafeorder/OrderDetailActivity.java
+++ app/src/main/java/com/number7/udemycafeorder/OrderDetailActivity.java
@@ -1,13 +1,36 @@
 package com.number7.udemycafeorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    private TextView textViewOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+
+        textViewOrder = findViewById(R.id.textViewOrder);
+        // intent - Получаем данные активности
+        Intent intent = getIntent();
+
+        /*
+        * Если intent содержит переменную order с предыдущей активности то
+        * присваиваем переменной order значения intent
+        * и выводим эти значения в textViewOrder
+        * иначе
+        * Возвращаемся на окно регестрации
+        * */
+        if(intent.hasExtra("order")){
+            String order = intent.getStringExtra("order");
+            textViewOrder.setText(order);
+        }else{
+            Intent backToLogin = new Intent(this, MainActivity.class);
+            startActivity(backToLogin);
+        }
     }
 }
